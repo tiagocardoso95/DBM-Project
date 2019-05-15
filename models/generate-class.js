fdfc44e2046b458bda104644cb9e1119f5b03ab4
tiagocardoso95 <tcardoso13@gmail.com>
2019-05-15 12:18:14 +0100

    Project setup, schema and class generation

---
+++ models/generate-class.js
@@ -0,0 +1,26 @@
+const mustache = require("mustache");
+var fs = require('fs');
+
+function generateClass(schema) {
+    var classProps = Object.keys(schema.properties);
+    var requiredProps = schema.required;
+    var view = {
+        classTitle: schema.title,
+        classProperties: classProps.join(),
+        classConstructor: function () {
+            var props = "";
+            classProps.forEach(key => {
+                props += "\n\t\tthis." + key + "=" + key + ";";
+
+                if (!requiredProps.includes(key))
+                    props += "\n\t\tObject.defineProperty(this, \"" + key + "\",{\n\t\t\t enumerable: false \n\t\t});\n";
+            });
+            return props;
+        }
+    }
+    var template = fs.readFileSync("models/class.mustache").toString();
+    var output = mustache.render(template, view);
+    fs.writeFileSync("publish/Models/" + view.classTitle + ".js", output);
+}
+
+module.exports.generateClass = generateClass;
