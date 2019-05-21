6d33a3a0c7983f77bfeca7f698dfb4e15f878942
Tiago Cardoso <tcardoso13@gmail.com>
2019-05-21 16:52:45 +0100

    folders + class generation

--- Server/server.js
+++ Server/server.js
@@ -8,14 +8,80 @@ var dbGenerator = require('../database/generate-database');
 var actorSchema = require("../schemas/actor-schema.json"); 
 
 module.exports = {
-    generateClasses(){
-        var schemas = Object.keys(config.schemas);
-        for(var i=0; i<schemas.length; i++){
-            var schema = JSON.parse(fs.readFileSync(schemas[i].path));
-            genClass.generateClass(schema);
-            console.log("%s"+".js generated!",schema[i].name);
-        }
-        
+    deleteFolders() {
+        del(['./publish/Controllers'], {
+            force: true
+        }).then(paths => {
+            console.log('Deleted files and folders:\n', paths.join('\n'))
+        });
+        del(['./publish/Models'], {
+            force: true
+        }).then(paths => {
+            console.log('Deleted files and folders:\n', paths.join('\n'))
+        });
+        del(['./publish/Public'], {
+            force: true
+        }).then(paths => {
+            console.log('Deleted files and folders:\n', paths.join('\n'))
+        });
+        del(['./publish/Views'], {
+            force: true
+        }).then(paths => {
+            console.log('Deleted files and folders:\n', paths.join('\n'))
+        });
+        del(['./publish/Database'], {
+            force: true
+        }).then(paths => {
+            console.log('Deleted files and folders:\n', paths.join('\n'))
+        });
+        fs.unlink('./publish/index.js', (err) => {
+            if (err) console.log(err);
+            console.log('File index.js deleted');
+        });
+    },
+    generateFolders() {
+        mkdirp('./publish/Controllers', (err) => {
+            if (err) console.log(err);
+            console.log('Controllers directory created.');
+        });
+        mkdirp('./publish/Models', (err) => {
+            if (err) console.log(err);
+            console.log('Models directory created.');
+        });
+        mkdirp('./publish/Public/Css', (err) => {
+            if (err) console.log(err);
+            else console.log('css dir created')
+        });
+        mkdirp('./publish/Public/Images', (err) => {
+            if (err) console.log(err);
+            else console.log('images dir created')
+        });
+        mkdirp('./publish/Public/Js', (err) => {
+            if (err) console.log(err);
+            else console.log('js dir created')
+        });
+        mkdirp('./publish/Views', (err) => {
+            if (err) console.log(err);
+            console.log('Views directory created.');
+        });
+        mkdirp('./publish/Database', (err) => {
+            if (err) console.log(err);
+            console.log('Database directory created.');
+        });
+    },
+    generateClasses() {
+        fs.readdir(path.resolve(config.schemaFolder), function (err, fileNames) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            fileNames.forEach(function (fileName) {
+                var schema = JSON.parse(fs.readFileSync(path.resolve(config.schemaFolder) + "/" + fileName));
+                genClass.generateClass(schema);
+                console.log("%s" + ".js generated!", schema.title);
+            });
+        });
+
     },
     generateDB(){
         //dbGenerator.generate(config.dbName,actorSchema);
